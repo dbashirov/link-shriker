2c75b438ab17c45898b7747bc6650c0710f85255
DamirBashirov <dam.bashirov@gmail.com>
2022-12-15 17:55:34 +0300

    dev

--- internal/shorten/service_test.go
+++ internal/shorten/service_test.go
@@ -1,16 +1,22 @@
 package shorten_test
 
 import (
+	"context"
 	"testing"
 
+	"github.com/dbashirov/link-shrinker/internal/model"
 	"github.com/dbashirov/link-shrinker/internal/shorten"
 	shortening "github.com/dbashirov/link-shrinker/internal/storage/shortering"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSerivice_Shortne(t *testing.T) {
 	t.Run("generates shortening for a given URL", func(t *testing.T) {
 		var (
-			svc = shorten.NewService(shortening.NewInMemory())
+			svc   = shorten.NewService(shortening.NewInMemory())
+			input = model.ShortenInput{RawURL: "https://www.google.com"}
 		)
+		shortening, err := svc.Shorten(context.Background(), input)
+		require.NoError(t, err)
 	})
 }
