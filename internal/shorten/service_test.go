d1fb8b6a392ec190f9bd6a7d5ca4616c95b823c0
DamirBashirov <dam.bashirov@gmail.com>
2022-12-19 09:54:36 +0300

    dev

--- internal/shorten/service_test.go
+++ internal/shorten/service_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSerivice_Shortne(t *testing.T) {
+func TestSerivice_Shorten(t *testing.T) {
 	t.Run("generates shortening for a given URL", func(t *testing.T) {
 		var (
 			svc   = shorten.NewService(shortening.NewInMemory())
@@ -22,5 +22,28 @@ func TestSerivice_Shortne(t *testing.T) {
 
 		assert.NotEmpty(t, shortening.Identifier)
 		assert.Equal(t, "https://www.google.com", shortening.OriginalURL)
+		assert.NotZero(t, shortening.CreatedAt)
+	})
+
+	t.Run("uses custom identifier if provided", func(t *testing.T) {
+		const identifier = "google"
+		var (
+			svc   = shorten.NewService(shortening.NewInMemory())
+			input = model.ShortenInput{
+				RawURL:     "https://www.google.com",
+				Identifier: identifier,
+			}
+		)
+		shortening, err := svc.Shorten(context.Background(), input)
+		require.NoError(t, err)
+
+		assert.Equal(t, identifier, shortening.Identifier)
+		assert.Equal(t, "https://www.google.com", shortening.OriginalURL)
+		assert.NotZero(t, shortening.CreatedAt)
+	})
+
+	t.Run("returns error if identifier is already taken", func(t *testing.T) {
+		const identifier = "google"
+		var ()
 	})
 }
