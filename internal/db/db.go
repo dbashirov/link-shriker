681664192b38b08f7725be0fef7f576d0eeaf5b3
DamirBashirov <dam.bashirov@gmail.com>
2022-12-20 10:05:03 +0300

    connect to mongo db

---
+++ internal/db/db.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type DB struct {
+	client *mongo.Client
+}
+
+func Conect(ctx context.Context, dsn string)(*DB, error) {
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(dsn))
+	if err != nil {
+		return nil, err
+	}
+
+	return &DB{client: client}, nil
+}
+
+func (d *DB) Client() *mongo.Client {
+	return d.client
+}
+
+func (d *DB) Close(ctx context.Context) error {
+	return d.client.Disconnect(ctx)
+}
